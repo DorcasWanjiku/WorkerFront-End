f835af310a83456a7748448ba5b5e02a02b4ecb2
ShemBrooklyne <shemnyatti@gmail.com>
2021-01-10 15:37:11 +0300

    page routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UserSigninComponent } from './user-signin/user-signin.component';
+import { UserSignupComponent } from './user-signup/user-signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'nav-bar', component:NavBarComponent},
+  {path: 'landing-page', component:LandingPageComponent},
+  {path: 'user-signup', component:UserSignupComponent},
+  {path: 'user-signin', component:UserSigninComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
